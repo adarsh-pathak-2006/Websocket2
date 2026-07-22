7187a7c326eba20bb83a13ab90abe8fd917e7b24
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-22 18:38:38 +0530

    made some changes

--- core/consumer.py
+++ core/consumer.py
@@ -2,6 +2,7 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 
+
 class MainConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
@@ -10,6 +11,6 @@ class MainConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         print(text_data)
 
-
+       
     async def disconnect(self, close_code):
         print(f"connection disconnected- {close_code}")
--- core/models.py
+++ core/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class MainDb(models.Model):
-    user=models.ForeignKey(User, on_delete=models.CASCADE)
     message=models.TextField()
     time=models.DateTimeField(auto_now_add=True)
 
