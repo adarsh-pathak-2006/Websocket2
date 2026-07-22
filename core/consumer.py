506e0985ee0e5b8966625e45432409fdf5b19447
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-22 18:33:20 +0530

    fixed an app bug and changed the model

--- core/consumer.py
+++ core/consumer.py
@@ -8,13 +8,8 @@ class MainConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def receive(self, text_data):
-        data = json.loads(text_data)
+        print(text_data)
 
-        await self.send(
-            text_data=json.dumps({
-                "message": data.get("message")
-            })
-        )
 
     async def disconnect(self, close_code):
         print(f"connection disconnected- {close_code}")
