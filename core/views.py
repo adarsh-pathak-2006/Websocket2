0471a0fe46bd2cb7f02e59265b0bea88de904222
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-22 12:10:30 +0530

    added gitignore

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from core.forms import RegisterForm, LoginForm
+from django.contrib.auth.models import User
+from django.views import View
 
-# Create your views here.
+
+class RegisterView(View):
+    def get(self, request):
+        form=RegisterForm()
+        return render(request, 'register.html', { 'form':form })
