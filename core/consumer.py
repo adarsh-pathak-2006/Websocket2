9d4c2ba2d195d5d40d8ceaf5639218953e309a15
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-22 19:48:46 +0530

    added database too..where the data can be logged on

--- core/consumer.py
+++ core/consumer.py
@@ -1,6 +1,6 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-
+from core.models import MainDb
 
 
 class MainConsumer(AsyncWebsocketConsumer):
@@ -10,7 +10,8 @@ class MainConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data):
         print(text_data)
-
+        await MainDb.objects.acreate(message=text_data)
+        await self.send(text_data="data saved in the database")
        
     async def disconnect(self, close_code):
         print(f"connection disconnected- {close_code}")
